d0318b7f9ced8c4e13e0004b598684e8e7dc195f
Tony Azar <tony.azarr@gmail.com>
2017-12-06 22:55:21 +0100

    typo

--- highscores/server.js
+++ highscores/server.js
@@ -17,7 +17,6 @@ var express = require('express'),
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/HighScoresdb', {useMongoClient: true}); 
-  useMongoClient: true,
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
